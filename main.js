d57f110c71309c48fdc0955c109b7123d3a27f6a
muhallilahnaf <muhallilahnaf@gmail.com>
2021-04-07 00:42:53 +0600

    initial

---
+++ main.js
@@ -0,0 +1,13 @@
+const editor = document.getElementById('editor')
+const toolbar = document.getElementById('toolbar')
+const toolbarSections = document.getElementsByClassName('toolbar-section')
+
+
+console.log(toolbar.clientHeight)
+
+// if (toolbar.clientHeight > 28) {
+
+//     for (const section of toolbarSections) {
+//         section.style.
+//     }
+// }
